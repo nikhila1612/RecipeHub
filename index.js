ad33013dbe9a8ae9108e97fbe034339f60772f0f
Nikhila M <84263617+nikhila1612@users.noreply.github.com>
2024-03-14 15:16:53 +0530

    add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 
-
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/", (req, res) => {
