5d95cf7cf333ffeeaa4ed07916f7a598b111196a
Nikhila M <84263617+nikhila1612@users.noreply.github.com>
2024-03-14 15:40:43 +0530

    add

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import axios from "axios";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
