ca6e05452608c6caf93ecd865302b7b8c7583d11
Nikhila M <84263617+nikhila1612@users.noreply.github.com>
2024-03-14 15:36:51 +0530

    add

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.set("views", "./views");
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     const content = "Discover Your Culinary Adventure: Welcome to Recipe Hub!"
     const home="Latest Meals"
