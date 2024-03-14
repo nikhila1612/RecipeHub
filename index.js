0b0a3a5ec3cd9c7b9045fb68ebc879d3ac6dedd2
Nikhila M <84263617+nikhila1612@users.noreply.github.com>
2024-03-14 15:31:56 +0530

    add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.set("views", "views");
+app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
