e0a74b901fa24af05047d6756b15bb36e1ee60a4
Nikhila M <84263617+nikhila1612@users.noreply.github.com>
2024-03-14 14:42:41 +0530

    add files

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.get("/", (req, res) => {
+    const content = "Discover Your Culinary Adventure: Welcome to Recipe Hub!"
+    const home="Latest Meals"
+    res.render("index", { image: null, instructions: null,foodName: null, content, home});
+});
+
+// Handle form submission
+app.post("/", async (req, res) => {
+    try {
+        const itemName = req.body.item;
+       
+        // Fetch data from MealDB API
+        const response = await axios.get(
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${itemName}`
+        );
+        const meal = response.data.meals[0];
+       
+        if (!meal) {
+            return res.render("index", { image: null, instructions: null,foodName:null, ingredients:null,content:"Sorry, No Meal Matches That Name! Feel Free to Try Something Else!",home:null});
+        }
+
+        const image = meal.strMealThumb;
+        const instructions = meal.strInstructions;
+        const foodName = meal.strMeal; 
+        const ingredients = [];
+        for (let i = 1; i <= 20; i++) {
+            if (meal[`strIngredient${i}`]) {
+                ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+            } else {
+                break;
+            }
+        }
+        res.render("index", { image, instructions,foodName,ingredients,content:null,home:null });
+    } catch (error) {
+        console.error("Error fetching meal:", error);
+        res.render("index", { image: null, instructions: null, foodName: null, ingredients:null,content:"Sorry, No Meal Matches That Name! Feel Free to Try Something Else!",home:null});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
